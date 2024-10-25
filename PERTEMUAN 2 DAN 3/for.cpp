f533bfe3a2b859c4ee68dda9b1af6bfacb320ea4
rhegina-rizky-sabangga <langit22062006@gmail.com>
2024-10-25 16:36:08 +0700

    Add files via upload

---
+++ PERTEMUAN 2 DAN 3/for.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+ int nilai_awal , nilai_akhir;
+
+//meminta input dari user 
+cout <<"Masukan nilai awal";
+cin >> nilai_awal;
+cout <<"Maukan nilai akhir";
+cin >>nilai_akhir;
+
+// perulangan nilai awal - nilai akhir 
+
+for(int i = nilai_awal; i <= nilai_akhir; i++) {
+    cout << i << ".classyscience.iu"<< endl;
+
+
+
+}
+return 0;
+
+
+
+} // namespace
